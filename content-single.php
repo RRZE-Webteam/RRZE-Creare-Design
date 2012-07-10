e8d7dc4e151b52d13ccecbc0a80eb78313f8e9fa
Natalia <Manager@MICROSO-N8TS1H2.rrze.uni-erlangen.de>
2012-07-10 16:06:10 +0200

    Artikel-Content angepasst: divs zum bearbeiten, prev/next-pfeilen

--- content-single.php
+++ content-single.php
@@ -3,10 +3,10 @@
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
         <header class="entry-header">
-            <h2><?php the_title(); ?></h2>
+           
             <?php if ('post' == get_post_type()) : ?>
                 <div class="entry-meta">
-                    <?php echo _rrze_posted_on(); ?>
+                    <p><?php echo _rrze_posted_on(); ?></p>
                 </div>
             <?php endif; ?>   
         </header>
@@ -18,21 +18,21 @@
 
         <footer class="entry-meta">
             <div class="ym-wbox">
-                <?php
+              <p>  <?php
                 $categories_list = get_the_category_list(', ');
 
                 $tag_list = get_the_tag_list('', ', ');
                 if ('' != $tag_list) {
-                    $utility_text = __('This entry was posted in %1$s and tagged %2$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', '_rrze' );
+                    $utility_text = __('This entry was posted in %1$s and tagged %2$s by <a href="%6$s">%5$s</a>. <br/>Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', '_rrze' );
                 } elseif ('' != $categories_list) {
-                    $utility_text = __('This entry was posted in %1$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', '_rrze' );
+                    $utility_text = __('This entry was posted in %1$s by <a href="%6$s">%5$s</a>. <br/>Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', '_rrze' );
                 } else {
-                    $utility_text = __('This entry was posted by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', '_rrze' );
+                    $utility_text = __('This entry was posted by <a href="%6$s">%5$s</a>. <br/>Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', '_rrze' );
                 }
 
                 printf($utility_text, $categories_list, $tag_list, esc_url(get_permalink()), the_title_attribute('echo=0'), get_the_author(), esc_url(get_author_posts_url(get_the_author_meta('ID'))));
-                ?>
-                <?php edit_post_link(__('Edit', '_rrze' ), '<span class="edit-link">', '</span>'); ?>
+                ?></p>
+                <p class="edit"><?php edit_post_link(__('Edit', '_rrze' ), '<span class="edit-link">', '</span>'); ?></p>
             </div>
         </footer>
 
